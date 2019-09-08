9fd060a6d4b82272c0f095bf25e60d3cf51b2101
Magnus Hovland Hoff <maghoff@gmail.com>
2019-09-08 20:41:50 +0200

    A touch of modularization

--- src/lib.rs
+++ src/lib.rs
@@ -1,144 +1,15 @@
-use cgmath::{prelude::*, vec2};
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader};
 
+mod renderer;
 mod segment;
+mod webgl;
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let context = webgl::get_context("canvas")?;
+    let renderer = renderer::Renderer::new(&context)?;
 
-    let context = canvas
-        .get_context("webgl")?
-        .unwrap()
-        .dyn_into::<WebGlRenderingContext>()?;
-
-    let vert_shader = compile_shader(
-        &context,
-        WebGlRenderingContext::VERTEX_SHADER,
-        r#"
-            attribute vec4 position;
-            void main() {
-                gl_Position = position;
-            }
-        "#,
-    )?;
-    let frag_shader = compile_shader(
-        &context,
-        WebGlRenderingContext::FRAGMENT_SHADER,
-        r#"
-            void main() {
-                gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-            }
-        "#,
-    )?;
-    let program = link_program(&context, &vert_shader, &frag_shader)?;
-    context.use_program(Some(&program));
-
-    let mut vertices = vec![];
-
-    let line = segment::Segment::Line {
-        start: vec2(-0.7, 0.),
-        dir: vec2(2., 1.).normalize(),
-        len: 1.4,
-    };
-    line.generate_geometry(&mut vertices);
-
-    let arc = segment::Segment::Arc {
-        center: vec2(0.5, 0.),
-        r: 0.3,
-        dir: 1.,
-        len: 1.4,
-        start_ang: 0.,
-    };
-    arc.generate_geometry(&mut vertices);
-
-    let buffer = context.create_buffer().ok_or("failed to create buffer")?;
-    context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
-
-    unsafe {
-        // Safe, because we're not allocating memory until view is out of scope
-        let vert_array = js_sys::Float32Array::view(&vertices);
-
-        context.buffer_data_with_array_buffer_view(
-            WebGlRenderingContext::ARRAY_BUFFER,
-            &vert_array,
-            WebGlRenderingContext::STATIC_DRAW,
-        );
-    }
-
-    context.vertex_attrib_pointer_with_i32(0, 2, WebGlRenderingContext::FLOAT, false, 0, 0);
-    context.enable_vertex_attrib_array(0);
-
-    context.clear_color(0.0, 0.0, 0.0, 0.0);
-    context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
-
-    context.draw_arrays(
-        WebGlRenderingContext::TRIANGLE_STRIP,
-        0,
-        (vertices.len() / 2) as i32,
-    );
+    renderer.render_scene()?;
 
     Ok(())
-}
-
-pub fn compile_shader(
-    context: &WebGlRenderingContext,
-    shader_type: u32,
-    source: &str,
-) -> Result<WebGlShader, String> {
-    let shader = context
-        .create_shader(shader_type)
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
-    context.shader_source(&shader, source);
-    context.compile_shader(&shader);
-
-    if context
-        .get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(shader)
-    } else {
-        Err(context
-            .get_shader_info_log(&shader)
-            .unwrap_or_else(|| String::from("Unknown error creating shader")))
-    }
-}
-
-pub fn link_program(
-    context: &WebGlRenderingContext,
-    vert_shader: &WebGlShader,
-    frag_shader: &WebGlShader,
-) -> Result<WebGlProgram, String> {
-    let program = context
-        .create_program()
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
-
-    context.attach_shader(&program, vert_shader);
-    context.attach_shader(&program, frag_shader);
-    context.link_program(&program);
-
-    if context
-        .get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(program)
-    } else {
-        Err(context
-            .get_program_info_log(&program)
-            .unwrap_or_else(|| String::from("Unknown error creating program object")))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
---
+++ src/renderer.rs
@@ -0,0 +1,94 @@
+use cgmath::{prelude::*, vec2};
+use wasm_bindgen::prelude::*;
+use web_sys::{WebGlProgram, WebGlRenderingContext};
+use crate::webgl;
+use crate::segment;
+
+pub struct Renderer<'a> {
+    context: &'a WebGlRenderingContext,
+    program: WebGlProgram,
+}
+
+fn build_shader_program(context: &WebGlRenderingContext) -> Result<WebGlProgram, JsValue> {
+    let vert_shader = webgl::compile_shader(
+        &context,
+        WebGlRenderingContext::VERTEX_SHADER,
+        r#"
+            attribute vec4 position;
+            void main() {
+                gl_Position = position;
+            }
+        "#,
+    )?;
+    let frag_shader = webgl::compile_shader(
+        &context,
+        WebGlRenderingContext::FRAGMENT_SHADER,
+        r#"
+            void main() {
+                gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+            }
+        "#,
+    )?;
+    let program = webgl::link_program(&context, &vert_shader, &frag_shader)?;
+
+    Ok(program)
+}
+
+impl<'a> Renderer<'a> {
+    pub fn new(context: &WebGlRenderingContext) -> Result<Renderer, JsValue> {
+        Ok(Renderer {
+            context,
+            program: build_shader_program(context)?,
+        })
+    }
+
+    pub fn render_scene(&self) -> Result<(), JsValue> {
+        self.context.use_program(Some(&self.program));
+
+        let mut vertices = vec![];
+
+        let line = segment::Segment::Line {
+            start: vec2(-0.7, 0.),
+            dir: vec2(2., 1.).normalize(),
+            len: 1.4,
+        };
+        line.generate_geometry(&mut vertices);
+
+        let arc = segment::Segment::Arc {
+            center: vec2(0.5, 0.),
+            r: 0.3,
+            dir: 1.,
+            len: 1.4,
+            start_ang: 0.,
+        };
+        arc.generate_geometry(&mut vertices);
+
+        let buffer = self.context.create_buffer().ok_or("failed to create buffer")?;
+        self.context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+
+        unsafe {
+            // Safe, because we're not allocating memory until view is out of scope
+            let vert_array = js_sys::Float32Array::view(&vertices);
+
+            self.context.buffer_data_with_array_buffer_view(
+                WebGlRenderingContext::ARRAY_BUFFER,
+                &vert_array,
+                WebGlRenderingContext::STATIC_DRAW,
+            );
+        }
+
+        self.context.vertex_attrib_pointer_with_i32(0, 2, WebGlRenderingContext::FLOAT, false, 0, 0);
+        self.context.enable_vertex_attrib_array(0);
+
+        self.context.clear_color(0.0, 0.0, 0.0, 0.0);
+        self.context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+
+        self.context.draw_arrays(
+            WebGlRenderingContext::TRIANGLE_STRIP,
+            0,
+            (vertices.len() / 2) as i32,
+        );
+
+        Ok(())
+    }
+}
