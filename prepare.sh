c023278ef651108b8c2a10c658ae79032b7c360a
Johannes Hoff <johshoff@gmail.com>
2019-09-09 21:36:02 +0200

    Install wasm-pack as a dependency

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 npm install webpack webpack-cli
+
+# install wasm-pack if it's not already in the path
+which wasm-pack >/dev/null || (curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh)
