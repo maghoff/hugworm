ab5b481fd3dd4e6e92dd79f329a89b4c9a6587b9
Johannes Hoff <johshoff@gmail.com>
2019-09-14 16:13:04 +0200

    The famous road renderer

--- src/frag.f.glsl
+++ src/frag.f.glsl
@@ -12,5 +12,15 @@ void main() {
             smoothstep(      edge_thickness,       edge_thickness - edge_smoothness, uv.x)  // left
         );
 
-    gl_FragColor = vec4(0.0, 0.0, 0.0, max(0.1, edge));
+    float middle =
+            smoothstep(0.5 - (edge_thickness / 2.0), 0.5 - (edge_thickness / 2.0 - edge_smoothness), uv.x) *
+            smoothstep(0.5 + (edge_thickness / 2.0 + edge_smoothness), 0.5 + (edge_thickness / 2.0), uv.x);
+
+    float marking = middle * smoothstep(0.8, 0.9, abs(sin(uv.y * 20.0)));
+
+    vec3 color = edge    * vec3(0.85, 0.89, 0.86) +
+                 marking * vec3(0.92, 0.90, 0.60) +
+                 (1.0 - marking - edge) * vec3(0.58, 0.62, 0.62);
+
+    gl_FragColor = vec4(color, 1.0);
 }
