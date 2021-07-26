84223ad0f35b4deb26633c967aae22ca14374c50
Magnus Hoff <maghoff@gmail.com>
2021-07-26 19:16:13 +0200

    Restructure event handler.
    Also: Drive-by reformatting with cargo fmt

--- src/platform_opengl/mod.rs
+++ src/platform_opengl/mod.rs
@@ -1,10 +1,15 @@
-use glium::{glutin, glutin::{event::VirtualKeyCode, event_loop::ControlFlow::{self, Exit}}};
+use glium::{
+    glutin,
+    glutin::{
+        event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+        event_loop::ControlFlow::{self, Exit},
+    },
+};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let event_loop = glutin::event_loop::EventLoop::new();
-    let window = glutin::window::WindowBuilder::new().with_inner_size(
-        glium::glutin::dpi::LogicalSize::new(500, 500)
-    );
+    let window = glutin::window::WindowBuilder::new()
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(500, 500));
     let context = glutin::ContextBuilder::new();
     let display = glium::Display::new(window, context, &event_loop)?;
 
@@ -18,22 +23,23 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     event_loop.run(move |event, _target, control_flow| {
         *control_flow = ControlFlow::Wait;
 
-        match event {
-            glutin::event::Event::WindowEvent { event, .. } => match event {
-                glutin::event::WindowEvent::KeyboardInput { input, .. }
-                    if input.state == glutin::event::ElementState::Pressed =>
-                {
-                    if let Some(keycode) = input.virtual_keycode {
-                        match keycode {
-                            VirtualKeyCode::Escape => *control_flow = Exit,
-                            _ => {},
-                        };
-                    }
-                }
-                glutin::event::WindowEvent::CloseRequested => *control_flow = Exit,
+        if let Event::WindowEvent { event, .. } = event {
+            match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(keycode),
+                            ..
+                        },
+                    ..
+                } => match keycode {
+                    VirtualKeyCode::Escape => *control_flow = Exit,
+                    _ => {}
+                },
+                WindowEvent::CloseRequested => *control_flow = Exit,
                 _ => (),
-            },
-            _ => (),
-        };
+            };
+        }
     });
 }
