0c490386d850ce82dbc3a60074d2e23c4dced8a1
Johannes Hoff <johshoff@gmail.com>
2021-07-26 21:30:36 +0200

    Step simulation ahead at 60 Hz

--- src/platform_opengl/mod.rs
+++ src/platform_opengl/mod.rs
@@ -1,30 +1,40 @@
 mod renderer;
 
+use std::time::{Duration, Instant};
+
 use glium::{
     glutin,
     glutin::{
         event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
-        event_loop::ControlFlow::{self, Exit},
+        event_loop::ControlFlow,
     },
 };
 
 use crate::{platform_opengl::renderer::Renderer, scene::Scene};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // nice_mode only renders a frame whenever the simulation has stepped forward. However,
+    // for unknown reasons, this creates jitter. Even if a frame is always rendered per
+    // simulation step.
+    let nice_mode = false;
+
     let event_loop = glutin::event_loop::EventLoop::new();
     let window = glutin::window::WindowBuilder::new()
         .with_inner_size(glium::glutin::dpi::LogicalSize::new(500, 500));
     let context = glutin::ContextBuilder::new();
     let display = glium::Display::new(window, context, &event_loop)?;
 
-    let scene = Scene::new();
+    let mut scene = Scene::new();
+
+    let tick_length = Duration::from_micros(16667);
+    let mut next_tick = Instant::now() + tick_length;
 
     let renderer = Renderer::new(display)?;
 
-    event_loop.run(move |event, _target, control_flow| {
-        *control_flow = ControlFlow::Wait;
+    renderer.render_scene(&scene);
 
-        if let Event::WindowEvent { event, .. } = event {
+    event_loop.run(move |event, _target, control_flow| {
+        if let Event::WindowEvent { event, .. } = &event {
             match event {
                 WindowEvent::KeyboardInput {
                     input:
@@ -35,14 +45,47 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
                         },
                     ..
                 } => match keycode {
-                    VirtualKeyCode::Escape => *control_flow = Exit,
+                    VirtualKeyCode::Escape => {
+                        *control_flow = ControlFlow::Exit;
+                        return;
+                    }
                     _ => {}
                 },
-                WindowEvent::CloseRequested => *control_flow = Exit,
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
                 _ => (),
             };
         }
 
-        renderer.render_scene(&scene);
+        if let Event::MainEventsCleared = &event {
+            let mut ticks_ticked = 0;
+            let mut need_render = false;
+            let now = Instant::now();
+            while now >= next_tick {
+                next_tick += tick_length;
+                scene.update();
+                need_render = true;
+                ticks_ticked += 1;
+            }
+
+            if nice_mode {
+                if ticks_ticked > 0 {
+                    println!("--------------- {}", ticks_ticked);
+                }
+
+                if need_render {
+                    let before = Instant::now();
+                    renderer.render_scene(&scene);
+                    let duration = Instant::now() - before;
+                    println!("{:5.5}", duration.as_micros());
+                }
+
+                *control_flow = ControlFlow::WaitUntil(next_tick);
+            } else {
+                renderer.render_scene(&scene);
+            }
+        }
     });
 }
