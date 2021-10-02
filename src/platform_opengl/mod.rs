c6d99cace65865128f85b8376d408a7800df72d7
Magnus Hoff <maghoff@gmail.com>
2021-10-02 21:05:26 +0200

    Hard-code nice mode.
    Nice mode was only problematic due to power saving.

--- src/platform_opengl/mod.rs
+++ src/platform_opengl/mod.rs
@@ -14,14 +14,7 @@ use self::renderer::Renderer;
 use crate::{scene::Scene, TICKS_PER_SECOND};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut pargs = pico_args::Arguments::from_env();
-
     simple_logger::init_with_env()?;
-
-    // nice_mode only renders a frame whenever the simulation has stepped forward. However,
-    // for unknown reasons, this creates jitter. Even if a frame is always rendered per
-    // simulation step.
-    let nice_mode = pargs.contains("--nice");
 
     let event_loop = glutin::event_loop::EventLoop::new();
     let window = glutin::window::WindowBuilder::new()
@@ -71,32 +64,19 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         if let Event::MainEventsCleared = &event {
-            let mut ticks_ticked = 0;
             let mut need_render = false;
             let now = Instant::now();
             while now >= next_tick {
                 next_tick += tick_length;
                 scene.update();
                 need_render = true;
-                ticks_ticked += 1;
             }
 
-            if nice_mode {
-                if ticks_ticked > 0 {
-                    log::trace!("--------------- {}", ticks_ticked);
-                }
-
-                if need_render {
-                    let before = Instant::now();
-                    renderer.render_scene(&scene);
-                    let duration = Instant::now() - before;
-                    log::trace!("{:5.5}", duration.as_micros());
-                }
-
-                *control_flow = ControlFlow::WaitUntil(next_tick);
-            } else {
+            if need_render {
                 renderer.render_scene(&scene);
             }
+
+            *control_flow = ControlFlow::WaitUntil(next_tick);
         }
     });
 }
